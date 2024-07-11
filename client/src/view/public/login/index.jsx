15362cc859f3c81f1f688fa6af8889a7d5ce9027
JoshVComstock <josedanielveizaga@gmail.com>
2024-07-11 11:45:11 -0400

    estructura de carpetas

---
+++ client/src/view/public/login/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div classname="w-full bg-black h-full ">
+      <label>Hola</label>
+    </div>
+  );
+};
+
+export default Login;
