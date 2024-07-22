6dd9c877a54164efaef881e4a26998f3da661bd4
JoshVComstock <josedanielveizaga@gmail.com>
2024-07-22 16:58:10 -0400

    login

--- client/src/view/public/index.tsx
+++ client/src/view/public/index.tsx
@@ -1,7 +1,7 @@
 import { ROUTES } from "@/types/enums/Routes";
 import { useNavigate } from "react-router-dom";
 import Back from "@assets/LoginBack.png";
-
+import Logo from "@assets/LogoSistema.png";
 const Login = () => {
   const navigate = useNavigate();
 
@@ -11,44 +11,36 @@ const Login = () => {
 
   return (
     <div
-      className="min-h-screen flex items-center justify-start bg-cover bg-center"
+      className="w-full h-full flex items-center justify-start bg-cover bg-center"
       style={{ backgroundImage: `url(${Back})` }}
     >
-      <div className="w-full max-w-md p-8 space-y-8 rounded-lg ml-20">
-        <div className="text-center">
-          <img src="logo.png" alt="Logo" className="mx-auto h-12 w-auto" />
-        </div>
-        <div className="space-y-6">
-          <div className="text-center">
-            <h2 className="text-2xl font-bold text-gray-900">Login</h2>
+      <div className="w-full h-full p-8 flex flex-col space-y-8 ">
+        <img src={Logo} alt="Logo" className="h-[10%] w-[200px]" />
+        <div className="space-y-6 h-[70%] w-[600px] flex flex-col justify-center items-center ">
+          <div className="text-center m-5">
+            <h2 className="text-5xl font-bold text-primary">Login</h2>
           </div>
-          <form className="space-y-6">
+          <form className="space-y-6 w-[70%] ">
             <div>
-              <label htmlFor="email" className="sr-only">
-                Email or phone number
-              </label>
               <input
                 id="email"
                 name="email"
                 type="email"
                 autoComplete="email"
                 required
-                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
-                placeholder="Email or phone number"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary100"
+                placeholder="Usuario"
               />
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
-                placeholder="Password"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary100"
+                placeholder="Contraseña"
               />
             </div>
             <div className="flex items-center justify-between">
@@ -57,42 +49,34 @@ const Login = () => {
                   id="remember-me"
                   name="remember-me"
                   type="checkbox"
-                  className="h-4 w-4 text-primary focus:ring-teal-500 border-gray-300 rounded"
+                  className="h-4 w-4 text-primary focus:ring-primary100 border-gray-300 rounded"
                 />
                 <label
                   htmlFor="remember-me"
                   className="ml-2 block text-sm text-gray-900"
                 >
-                  Remember me
+                  Recuerdame
                 </label>
               </div>
               <div className="text-sm">
                 <a
                   href="#"
-                  className="font-medium text-primary hover:text-primary100"
+                  className="font-medium text-primary hover:border-b-2"
                 >
-                  Forgot your password?{" "}
+                  Olvidaste tu contraseña?
                 </a>
               </div>
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary hover:bg-primary100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary hover:bg-primary100 focus:outline-none"
                 onClick={handleInicio}
               >
                 Login
               </button>
             </div>
           </form>
-          <div className="text-center">
-            <a
-              href="#"
-              className="font-medium text-primary hover:text-primary100"
-            >
-              Sign up
-            </a>
-          </div>
         </div>
       </div>
     </div>
