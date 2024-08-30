98d26539378287c7e014c58f3c859adb3efccc7b
TANHEHANZ <hantach10@email.com>
2024-08-30 00:32:37 -0400

    createConfig

--- server/src/service-inventory/config/server.ts
+++ server/src/service-inventory/config/server.ts
@@ -1,4 +1,9 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const env = process.env.NODE_ENV || 'development';
+dotenv.config({ path: path.resolve(__dirname, `.env.${env}`) });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
