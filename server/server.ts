52d6368c571112bc541456352d5f9b8e45f978e2
TANHEHANZ <hantach10@email.com>
2024-09-04 14:50:24 -0400

    alias rutas

--- server/server.ts
+++ server/server.ts
@@ -2,10 +2,10 @@ import "module-alias/register";
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import ProductosRoutes from "./src/modules/inventary/routes/productos.routes";
-import UnidadesRoutes from "./src/modules/inventary/routes/unidades.routes";
-import CategoriaRoutes from "./src/modules/inventary/routes/categorias.routes";
-import LoteRoutes from "./src/modules/inventary/routes/lote.routes";
+import ProductosRoutes from "@routes/productos.routes";
+import UnidadesRoutes from "@routes/unidades.routes";
+import CategoriaRoutes from "@routes/categorias.routes";
+import LoteRoutes from "@routes/lote.routes";
 import { ROUTES } from "./src/common/utils/routes.enum";
 
 const env = process.env.NODE_ENV || "development";
